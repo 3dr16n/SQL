3695d748e9897c987eb4bd9ce4356642c16d1e71
3dr16n <3dr16n@gmail.com>
2025-01-27 20:57:33 +0800

    sync

---
+++ SQL_for_Non_ProgrammersExercise_Files/Data/Challenge.sql
@@ -0,0 +1,13 @@
+SELECT DISTINCT cust_type
+FROM sales;
+
+SELECT order_num, order_type, cust_type, order_total
+FROM sales
+ORDER BY order_total DESC;
+
+
+SELECT order_num, order_type, cust_type, order_total
+FROM sales
+WHERE cust_state IN ('Georgia', 'North Carolina')
+ORDER BY order_total DESC
+LIMIT 20;
