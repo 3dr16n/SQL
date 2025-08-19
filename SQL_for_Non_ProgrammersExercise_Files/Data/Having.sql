f3c266467a2ebe770219c00cecb6bbd0770001b2
Edrian <edrian@M4.local>
2025-08-19 21:14:52 +0800

    update

--- SQL_for_Non_ProgrammersExercise_Files/Data/Having.sql
+++
@@ -1,8 +0,0 @@
-SELECT cust_state,
-	SUM (order_total) AS sum_of_orders,
-	COUNT (*) AS order_count,
-	AVG(order_total) AS avg_order
-FROM sales
-GROUP BY cust_state
-HAVING avg_order < 200
-ORDER BY sum_of_orders DESC;
